8291fc73524326888149cab283908ee4fe901ff6
wangxiang <wangxiang@yixia.com>
2018-03-21 11:09:42 +0800

    first commit

--- SHPractice-NSOperation/ViewController.m
+++ SHPractice-NSOperation/ViewController.m
@@ -16,14 +16,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self demo4];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//NSOperation是个抽象类，不能直接使用。应该使用它的子类NSInvocationOperation 和 NSBlockOperation
+
+//Operation可以直接使用，不关联队列。但一般很少这么用
+- (void)demo1{
+    NSInvocationOperation *operation = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(downloadImage:) object:@"invacation"];
+    [operation start];
 }
 
+//NSInvocationOperation
+- (void)demo2{
+    NSInvocationOperation *operation = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(downloadImage:) object:@"invacation"];
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    [queue addOperation:operation];
+}
+
+
+//NSBlockOperation
+- (void)demo3{
+    NSBlockOperation *operation = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"%@",[NSThread currentThread]);
+    }];
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    [queue addOperation:operation];
+}
+
+
+//NSOperationQueue 会开启多个线程，不会顺序执行.
+//所以NSOperationQueue 是GCD异步+并发队列 的封装。
+- (void)demo4{
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    [queue addOperationWithBlock:^{
+        for(int i = 0; i < 10; i++){
+            NSLog(@"第1个任务%@",[NSThread currentThread]);
+        }
+    }];
+    
+    [queue addOperationWithBlock:^{
+        NSLog(@"第2个任务%@",[NSThread currentThread]);
+    }];
+    
+    [queue addOperationWithBlock:^{
+        NSLog(@"第3个任务%@",[NSThread currentThread]);
+    }];
+}
+
+- (void)downloadImage:(id)obj{
+    NSLog(@"%@",[NSThread currentThread]);
+}
 
 @end
